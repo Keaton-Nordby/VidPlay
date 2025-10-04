ec2bff7b3a7088a15c10808d8b7513a15ee2ca12
Keaton Nordby <keatonnordby4@gmail.com>
2025-10-04 14:33:29 -0500

    sign in using google

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import Image from "next/image";
-import styles from "./navbar.module.css"
 import Link from "next/link";
 
+import styles from "./navbar.module.css"
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+
+// closure 
+
 export default function Navbar() {
+
+    // user state
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user); 
+        })
+
+        return () => unsubscribe();
+    });
+
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
                 <Image width={100} height={70}
                     src="/logo.png" alt="VidPlay" />
             </Link>
+            <SignIn user={user}/>
         </nav>
     );
 }
